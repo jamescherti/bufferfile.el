6bcff99b4e78a83ffaa15f1ce95f4f0421db64b0
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-01-14 10:35:19 -0500

    Update copyright year

--- bufferfile.el
+++ bufferfile.el
@@ -1,6 +1,6 @@
 ;;; bufferfile.el --- Rename/Delete/Copy Files and Associated Buffers -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024-2025 James Cherti | https://www.jamescherti.com/contact/
+;; Copyright (C) 2024-2026 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
 ;; Version: 1.0.6
